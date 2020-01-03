afb8be04c5a4e398fdeabdc528d7d7dfd2d590a5
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2020-01-02 21:00:24 -0300

    responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Header from './components/Header'
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap';
 import LogoWhite from '../assets/img/logo-white.png';
 import Linkedin from '../assets/icons/linkedin.svg';
 import Behance from '../assets/icons/behance.svg';
@@ -11,29 +12,58 @@ import { Container } from '@material-ui/core'
 
 
 function Navigation () {
+    // const [navbarColor, setNavbarColor] = React.useState("navbar-transparent");
+    // React.useEffect(() => {
+    //   const updateNavbarColor = () => {
+    //     if (
+    //       document.documentElement.scrollTop > 380 ||
+    //       document.body.scrollTop > 380
+    //     ) {
+    //       setNavbarColor("nav-scroll");
+    //     } 
+    //     else if (
+    //       (document.documentElement.scrollTop > 201  || document.body.scrollTop > 201 ) && 
+    //       (document.documentElement.scrollTop < 379  || document.body.scrollTop < 379 )
+    //     ) {
+    //       setNavbarColor("");
+    //     } 
+    //     else if (
+    //       document.documentElement.scrollTop < 200 ||
+    //       document.body.scrollTop < 200
+    //     ) {
+    //       setNavbarColor("navbar-transparent");
+    //     }
+    //   };
+    //   window.addEventListener("scroll", updateNavbarColor);
+    //   return function cleanup() {
+    //     window.removeEventListener("scroll", updateNavbarColor);
+    //   };
+    // });
         return (
             // <nav className={"nav-bar " + navbarColor}>
-             <nav className="nav-bar navbar-transparent">
+             <nav className="">
                 <Container maxWidth='lg'>
-                    <div className="nav-content">
-                        <img src={LogoWhite} alt="logo" className="logo-nav"></img>
-                        <div className="nav-links">
-                            <Router>
-                                <Link to="/">INICIO</Link>
-                                <Link to="/about">CONÓCEME</Link>
-                                <Link to="/proyects">PORTAFOLIO</Link>
-                                <Link to="/contact">CONTACTO</Link>
-                                <Link to="/resume">CURRICULUM</Link>
-                            </Router>
-
-                        </div>
-                        <ul className="nav-socials">
-                            <li><a href="www.linkedin.com/in/fernanda-vega" rel='noopener'><img src={Linkedin} alt="linkedin"></img></a></li>
-                            <li><a href="https://www.behance.net/fernandabelen" rel='noopener'><img src={Behance} fill="white" alt="behance"></img></a></li>
-                            <li><a href="https://github.com/fernandabelenVL" rel='noopener'><img src={Github} fill="white" alt="github"></img></a></li>
-                        </ul>
-                    </div>
-                </Container>
+                <Navbar sticky="top" collapseOnSelect expand="lg" bg="transparent" className="nav-bar">
+                <Router>
+                <Navbar.Brand as={Link} to="/"><img src={LogoWhite} alt="logo" className="logo-nav"></img></Navbar.Brand>
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                  <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto nav-links">
+                      <Nav.Link as={Link} to="/">INICIO</Nav.Link>
+                      <Nav.Link as={Link} to="/about">CONÓCEME</Nav.Link>
+                      <Nav.Link as={Link} to="/proyects">PORTAFOLIO</Nav.Link>
+                      <Nav.Link as={Link} to="/contact">CONTACTO</Nav.Link>
+                      <Nav.Link as={Link} to="/resume">CURRICULUM</Nav.Link>
+                    </Nav>
+                    {/* <Nav className="nav-socials">
+                      <Nav.Link href="www.linkedin.com/in/fernanda-vega" rel='noopener'>><img src={Linkedin} alt="linkedin"></img></Nav.Link>
+                      <Nav.Link href="https://www.behance.net/fernandabelen" rel='noopener'><img src={Linkedin} alt="linkedin"></img></Nav.Link>
+                      <Nav.Link href="https://github.com/fernandabelenVL" rel='noopener'><img src={Linkedin} alt="linkedin"></img></Nav.Link>
+                    </Nav> */}
+                  </Navbar.Collapse>
+                  </Router>
+                </Navbar>
+            </Container>
 
                 {/* <Router>
                     <Switch>
